c50b7745b33a39d5cb9f7df6ba76943af7954cc5
+ <+>
2024-08-18 01:10:06 +0300

    committed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import "./index.css";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
+import routes from "./routes"; // Make sure routes.js is correctly set up
 
-const router = createBrowserRouter(routes)
+// Create the router using the defined routes
+const router = createBrowserRouter(routes);
+
+// Create the root element and render the application
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router}/>);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
